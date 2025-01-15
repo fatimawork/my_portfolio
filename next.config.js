551fdfaa768a84a2b930ef0820514529462f527a
Fatima El mourtaki <fatimaelmourtaki@gmail.com>
2025-01-15 21:14:04 +0100

    chages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    experimental: {
+      outputFileTracingExcludes: {
+        '*': [
+          '**/node_modules/**',
+          '**/.git/**',
+          '**/.next/**',
+        ],
+      },
+    },
+  };
+  
